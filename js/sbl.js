4bf0bc349b87227d56dce896728eb81961c03a2a
shengbolou <loushengbo@hotmail.com>
2016-02-04 19:57:59 -0500

    configure navbar toggle

--- js/sbl.js
+++ js/sbl.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
   var myHeight = $( window ).height();;
 
   $('.pageheader').css('height',myHeight+30);
-
+  $(".navbar-toggle").on("click", function () {
+      $(this).toggleClass("active");
+  });
 });

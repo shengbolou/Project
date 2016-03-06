4dd3cdf99626563f53aa8af7343e211162eeda86
shengbolou <loushengbo@hotmail.com>
2016-03-05 21:52:03 -0500

    improve

--- js/Spotify.js
+++ js/Spotify.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     var myHeight = $(window).height();
     var check = 0;
 
+    $('.page-header').css("height",myHeight);
+    $('.ph').css("height",myHeight);
 
     $('.navbar-toggle').click(function(){
       if(check == 0){
@@ -14,15 +16,62 @@ $(document).ready(function(){
           check = 0;
         }
     });
-    
+
     $('.collapsed-close').click(function(){
         $('.collapsed').velocity("transition.slideLeftOut",200);
     });
 
+    $('.collapsed').css('height',myHeight);
 
+    $('#s_up').hover(function(){
+      $(this).velocity({
+        color:'#5bc721'
+      })
+    },function(){
+      $(this).velocity({
+        color:'#bdbdbd'
+      })
+    });
+    
+    $('#s_in').hover(function(){
+      $(this).velocity({
+        color:'#5bc721'
+      })
+    },function(){
+      $(this).velocity({
+        color:'#bdbdbd'
+      })
+    });
 
-
-      $('.collapsed').css('height',myHeight);
+    $('#li a').hover(function(){
+      $(this).velocity({
+        color:'#5bc721'
+      })
+    },function(){
+      $(this).velocity({
+        color:'#ffffff'
+      })
+    });
+    $('#ph_btn').hover(function(){
+      $(this).velocity({
+        backgroundColor:'#5bc721'
+      },100)
+    },function(){
+      $(this).velocity({
+        backgroundColor:'#37ac38'
+      },100)
+    });
+    $('#ph_btn2').hover(function(){
+      $(this).velocity({
+        color:'#ffffff',
+        backgroundColor:'#000000'
+      },200)
+    },function(){
+      $(this).velocity({
+        color:'#000000',
+        backgroundColor:'#ffffff'
+      },200)
+    });
 
 
 
